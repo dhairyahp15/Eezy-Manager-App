f79871fcc2a152598a7a629f365fbd9228f019a7
w <shialzen@gmail.com>
2022-02-25 10:09:49 -0600

    Calendar created with a light and dark mode

---
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_night);
+    }
+
+}
